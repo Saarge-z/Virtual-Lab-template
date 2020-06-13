c5ddab4306256d197f3f53e662fba4af27e020e6
Saarge <razamohib01@gmail.com>
2020-06-13 17:11:27 +0530

    Initial commit

---
+++ about.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Team VLab</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php 
+    include ("header.php")    
+    ?>
+</head>
+<body>
+
+<div class="jumbotron">
+  <h1>About Us</h1>
+  <p>This is the about us page</p>
+</div>
+<section class="my-5">
+  <div class="py-3">
+    <h1 class="text-center"></h1>
+</div>
+<div class="container-fluid">
+  <div class="row">
+   <div class="col-lg-6 col-md-6 col-12" >
+     <img src="images/d.jpeg" class="img-fluid aboutimg">
+   </div>
+   <div class="col-lg-6 col-md-6 col-12" >
+     <h2 class="display-4">The Call isn't out there at all, it's inside me.</h2>
+     <p class="py-5">Like the tide slowly falling and rising.
+       I will carry you here in my heart you remind me.</p>
+       <a href="about.php" class="btn btn-success">Click Here</a>
+   </div>
+</div>
+</section>
+<?php 
+include ("footer.php")    
+?> 
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer>
+    <p class="p-3 bg-dark text-white text-center">...</h3>
+</footer>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<script src = "js/jquery.slim.min.js"></script>
+<script src = "js/bootsrap.bundle.min.js"></script>
+<script src = "js/bs-custom-file-input.min.js"></script>
+<script src = "js/simplebar.min.js"></script>
+<script src = "js/smooth-scroll.polyfills.min.js"></script>
+<script src = "js/parallax.min.js"></script>
+<!--Main Theme Script-->
+<script src = "js/theme.min.js"></script>
+<script src="js/clean-blog.min.js"></script>
